c9a1e9fb2deb29f106824828df73d69d51482d75
Paul Sadauskas <rando@theamazingrando.com>
2008-05-21 15:14:29 -0600

    Initial go

--- tasks/setup.rb
+++ tasks/setup.rb
@@ -8,6 +8,9 @@ end
 
 SITE = Webby.site
 
+SITE.host = 'theamazingrando.com'
+SITE.remote_dir = '/srv/www/sadauskas.us/htdocs'
+
 # Load the other rake files in the tasks folder
 Dir.glob('tasks/*.rake').sort.each {|fn| import fn}
 
